3e54afe4a53993854aa6bac0ecbb58f034178be3
Edward Vidal Jr <develone@sbcglobal.net>
2018-04-27 00:22:56 +0000

    added comments to tb_dut_uart_txrx.v tb_dut_uart_txrx_comb.v still not working heed to map wire i_TX_DV & wire [7:0] i_TX_Byte
    Please enter the commit message for your changes. Lines starting

--- MyHDL/tb_dut_uart_txrx.v
+++ MyHDL/tb_dut_uart_txrx.v
@@ -1,17 +1,32 @@
 
 `include "uart_tx.v" 
-module tb_uart_tx;
+module tb_uart_txrx;
 
  
   parameter c_CLOCK_PERIOD_NS = 10;
   parameter c_CLKS_PER_BIT    = 217;
-  parameter c_BIT_PERIOD      = 8600;
+  //The parameter c_BIT_PERIOD      = 8600;
+  //not required
+  //parameter c_BIT_PERIOD      = 8600;
+  //Used to generate 100MHz clock 
+  //using the parameter c_CLOCK_PERIOD_NS = 10;
   reg r_Clock = 0;
+
+  //Signal to start to the transfer on o_uart_tx
+  //During the transfer the o_TX_Active is hi
   reg r_TX_DV = 0;
   wire w_TX_Active, w_UART_Line;
+  //Signal to map to o_uart_tx
   wire w_TX_Serial;
+  //Signal to map to i_TX_Byte
   reg [7:0] r_TX_Byte = 0;
+  //Signal to map to o_RX_Byte
   wire [7:0] w_RX_Byte;
+  //for loop reg that allows
+  //clocks pass the detection of 
+  //w_RX_Byte to see that the r_TX_Done
+  reg [7:0] r_loop = 0;
+  
   
   uart_tx dut_tx(
     .i_Clock(r_Clock),
@@ -30,6 +45,8 @@ module tb_uart_tx;
   // Keeps the UART Receive input high (default) when
   // UART transmitter is not active
   assign w_UART_Line = w_TX_Active ? w_TX_Serial : 1'b1;
+  //Used to generate 100MHz clock
+  //which drives the simulation
   always
     #(c_CLOCK_PERIOD_NS/2) r_Clock <= !r_Clock;
   // Main Testing:
@@ -48,7 +65,13 @@ module tb_uart_tx;
         $display("Test Passed - Correct Byte Received");
       else
         $display("Test Failed - Incorrect Byte Received");
-  
+      //for loop reg that allows
+      //clocks pass the detection of 
+      //w_RX_Byte to see that the r_TX_Done      
+      for (r_loop = 0; r_loop <= 254 ; r_loop = r_loop + 1) begin
+      
+			@(posedge r_Clock);
+	  end  
       $finish();
     end
 
--- MyHDL/tb_dut_uart_txrx_comb.v
+++ MyHDL/tb_dut_uart_txrx_comb.v
@@ -6,20 +6,41 @@ module tb_uart_txrx;
   parameter c_CLOCK_PERIOD_NS = 10;
   parameter c_CLKS_PER_BIT    = 217;
   //parameter c_BIT_PERIOD      = 8600;
+  //r_loop used to increment the r_Clock
+  reg [11:0] r_loop;
+  
+  //used to drive the i_clk
   reg r_Clock = 0;
-  reg r_TX_DV = 0;
-  wire w_TX_Active, w_UART_Line;
+  
+  //used to connect the i_uart_rx
+  wire w_UART_Line; 
+  
+  //used to connect the w_TX_Serial
   wire w_RX_Serial;
-  reg [7:0] r_TX_Byte = 0;
-  wire [7:0] w_RX_Byte;
-  wire w_RX_DV;
+  
+  //used to connect the i_TX_DV
+  reg r_TX_DV = 0;
+  
   uart_txrx dut(
     .i_clk(r_Clock),
     .i_uart_rx(w_UART_Line),
     .o_uart_tx(w_TX_Serial)
   );
-  assign w_TX_Serial = 1'b1;
+ 
+  //wire i_TX_DV;
+  //wire [7:0] i_TX_Byte; 
+  
+  //wire i_TX_DV;
+  wire [7:0] i_TX_Byte;   
+  
+  //reg r_TX_DV = 0;
+  reg [7:0] r_TX_Byte = 0;
+  //wire [7:0] w_RX_Byte;
+
+  //assign w_RX_DV = 1'b0;
   assign w_TX_Active = 1'b1;
+  assign i_TX_DV = r_TX_DV;
+  //assign i_TX_DV = r_TX_DV ? r_TX_DV : 1'b0;
   // Keeps the UART Receive input high (default) when
   // UART transmitter is not active
   assign w_UART_Line = w_TX_Active ? w_TX_Serial : 1'b1;
@@ -35,13 +56,16 @@ module tb_uart_txrx;
       r_TX_Byte <= 8'h3F;
       @(posedge r_Clock);
       r_TX_DV <= 1'b0;
-      
+      for (r_loop = 0; r_loop <= 1736 ; r_loop =r_loop + 1) begin
+       @(posedge r_Clock);
+ 
+      end  
       // Check that the correct command was received
-      @(posedge w_RX_DV);
-      if (w_RX_Byte == 8'h3F)
-        $display("Test Passed - Correct Byte Received");
-      else
-        $display("Test Failed - Incorrect Byte Received");
+      //@(posedge w_RX_DV);
+      //if (w_RX_Byte == 8'h3F)
+        //$display("Test Passed - Correct Byte Received");
+      //else
+        //$display("Test Failed - Incorrect Byte Received");
   
       $finish();
     end
