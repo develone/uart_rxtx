8899c8a83ad6f3fed1af56f051b444f61535e47c
Edward Vidal Jr <develone@sbcglobal.net>
2018-04-28 00:40:16 +0000

    trying to get myhdl code working the same.

--- MyHDL/uart_txrx.sh
+++ MyHDL/uart_txrx.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 rm -f uart_loopback.blif
-yosys -q -p "synth_ice40 -blif uart_loopback.blif" uart_loopback.v 
-
-
-
+yosys -q -p "synth_ice40 -blif uart_loopback.blif" uart_loopback.v \
+uart_rx.v uart_tx.v
