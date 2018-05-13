d52eea7442195017075337d9340de50c64f3d6ad
Edward Vidal Jr <develone@sbcglobal.net>
2018-05-13 23:22:24 +0000

    new pins added not hanging up can sw fpga programs okay

---
+++ wbuart32/pptest/blink.c
@@ -0,0 +1,30 @@
+#include <wiringPi.h>
+//gcc -Wall -o blink blink.c -lwiringPi
+//sudo ./blink
+
+#  define RASPI_DIR 28 //BCM20 	PIN 38, GPIO.28 IOB_59  R3
+#  define RASPI_CLK 10 //BCM8 	PIN 24, GPIO.10, IOB_75	T8
+
+#  define RASPI_D0 27 //BCM16 	PIN 36, GPIO.27, IOB_63	R4
+#  define RASPI_D1 24 //BCM19 	PIN 35, GPIO.24, IOB_61	T3
+#  define RASPI_D2 0 //BCM17 	PIN 11, GPIO.0 ,IOB_94	T13
+ 
+#  define RASPI_D3 21 //BCM5   PIN  29,GPIO.21 ,IOB_73	T6
+ 
+#  define RASPI_D4 22 //BCM6    PIN 31,GPIO.22 ,IOB_69	T5
+#  define RASPI_D5 4 //BCM23 	PIN 16,GPIO.4, IOB_83	P9
+ 
+#  define RASPI_D6 5 //BCM24 	PIN 18, GPIO.5, IOB_79	T9
+#  define RASPI_D7 1  //BCM18 	pin 12, GPIO.1, IOB_89	T11
+#  define RASPI_D8 11 //BCM7 	PIN 26, GPIO.11, IOB_75	T7
+int main (void)
+{
+  wiringPiSetup () ;
+  pinMode (RASPI_DIR, OUTPUT) ;
+  for (;;)
+  {
+    digitalWrite (RASPI_DIR, HIGH) ; delay (500) ;
+    digitalWrite (RASPI_DIR,  LOW) ; delay (500) ;
+  }
+  return 0 ;
+}
--- wbuart32/pptest/host/netpport.cpp
+++ wbuart32/pptest/host/netpport.cpp
@@ -73,17 +73,23 @@ bool verbose = false;
 //
 // RPi GPIO #, connector pin #, schematic name, fpga pin #
 //
-#  define RASPI_D8   0 // PIN 11, GPIO.0,  IO219,       D5
-#  define RASPI_D7   1 // PIN 12, GPIO.1,  IO212,       D6
-#  define RASPI_D6   3 // PIN 15, GPIO.3,  IO209,       C6
-#  define RASPI_D5   4 // PIN 16, GPIO.4,  IO206,       C7
-#  define RASPI_D4  12 // PIN 19, MOSI,    RPI_SPI_MOSI,A6
-#  define RASPI_D3  13 // PIN 21, MISO,    RPI_SPI_MISO,A7
-#  define RASPI_D2  11 // PIN 26, CE1,     IO224,       D4
-#  define RASPI_D1  24 // PIN 35, GPIO.24, IO210,       D7
-#  define RASPI_D0  27 // PIN 36, GPIO.27, IO193,       D9
-#  define RASPI_DIR 28 // PIN 38, GPIO.28, IO191,       C9
-#  define RASPI_CLK 29 // PIN 40, GPIO.29, IO185,       C10
+ 
+
+#  define RASPI_DIR 28 //BCM20 	PIN 38, GPIO.28 IOB_59  R3
+#  define RASPI_CLK 10 //BCM8 	PIN 24, GPIO.10, IOB_75	T8
+
+#  define RASPI_D0 27 //BCM16 	PIN 36, GPIO.27, IOB_63	R4
+#  define RASPI_D1 24 //BCM19 	PIN 35, GPIO.24, IOB_61	T3
+#  define RASPI_D2 0 //BCM17 	PIN 11, GPIO.0 ,IOB_94	T13
+ 
+#  define RASPI_D3 21 //BCM5   PIN  29,GPIO.21 ,IOB_73	T6
+ 
+#  define RASPI_D4 22 //BCM6    PIN 31,GPIO.22 ,IOB_69	T5
+#  define RASPI_D5 4 //BCM23 	PIN 16,GPIO.4, IOB_83	P9
+ 
+#  define RASPI_D6 5 //BCM24 	PIN 18, GPIO.5, IOB_79	T9
+#  define RASPI_D7 1  //BCM18 	pin 12, GPIO.1, IOB_89	T11
+#  define RASPI_D8 11 //BCM7 	PIN 26, GPIO.11, IOB_75	T7
 
 #define	READ_FROM_ICO	0
 #define	WRITE_TO_ICO	1
